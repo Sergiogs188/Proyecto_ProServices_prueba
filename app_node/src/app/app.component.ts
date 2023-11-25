16075131bd8b148b7e4843f88b8a442db12809a6
Sergio GS <sergiogs188@gmail.com>
2023-11-25 11:59:00 -0300

    styles.css

--- app_node/src/app/app.component.ts
+++ app_node/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app/styles/app.component.css']
+  styleUrls: ['./app/styles/app.component.css'],
 })
 export class AppComponent {
   title = 'app_node';
